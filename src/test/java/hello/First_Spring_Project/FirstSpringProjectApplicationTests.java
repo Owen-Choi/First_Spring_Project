b75c497a3f5984cac2571b0ecca01d8baee8a7f0
Owen-Choi <oldstyle482@gmail.com>
2022-01-10 17:33:16 +0900

    Initial commit

---
+++ src/test/java/hello/First_Spring_Project/FirstSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.First_Spring_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
