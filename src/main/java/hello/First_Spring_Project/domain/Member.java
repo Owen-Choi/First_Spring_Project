e05b26cfdcd26fc43c9eb4fa1412a8b018863ae1
Owen-Choi <oldstyle482@gmail.com>
2022-07-08 00:58:50 +0900

    JPA 입문

--- src/main/java/hello/First_Spring_Project/domain/Member.java
+++ src/main/java/hello/First_Spring_Project/domain/Member.java
@@ -1,7 +1,20 @@
 package hello.First_Spring_Project.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+// Entity 태그를 달면 이제부터 JPA가 관리하는 클래스라고 인식이 된다.
 public class Member {
+    // id가 pk이면서 자동으로 증가한다. 전마협에서 user_id 같은 맥락이다.
+    // 이런 데이터를 IDENTITY라고 한다.
+    // 이렇게 활용되는 데이터에 @Id, @GeneratedValue(strategy = GenerationType.IDENTITY)를 붙여준다.
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    //@Column(name="username") 이라고 해두면 데이터베이스에서 username이라는 칼럼과 매핑이 된다.
     private String name;
 
     public long getId() {
---
+++ src/main/java/hello/First_Spring_Project/repository/JpaMemberRepository.java
@@ -0,0 +1,37 @@
+package hello.First_Spring_Project.repository;
+
+import hello.First_Spring_Project.domain.Member;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+public class JpaMemberRepository implements MemberRepository{
+
+    // JPA는 엔티티 매니저를 통해서 모든 것이 동작한다.
+    private final EntityManager em;
+
+    public JpaMemberRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public Member save(Member member) {
+        return null;
+    }
+
+    @Override
+    public Optional<Member> findById(long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return null;
+    }
+}
